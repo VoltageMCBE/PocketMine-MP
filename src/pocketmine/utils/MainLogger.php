b1a965606f25cc5ffba2b9f4679337c9f8f9a887
TheCGaming <tiidezorder@yahoo.com>
2017-12-04 06:46:20 -1000

    Update MainLogger.php

--- src/pocketmine/utils/MainLogger.php
+++ src/pocketmine/utils/MainLogger.php
@@ -210,7 +210,7 @@ class MainLogger extends \AttachableThreadedLogger{
 			$threadName = (new \ReflectionClass($thread))->getShortName() . " thread";
 		}
 
-		$message = TextFormat::toANSI(TextFormat::AQUA . "[" . date("H:i:s", $now) . "] " . TextFormat::RESET . $color . "[" . $threadName . "/" . $prefix . "]:" . " " . $message . TextFormat::RESET);
+		$message = TextFormat::toANSI(TextFormat::AQUA . "VoltageNetwork: " . $message . TextFormat::RESET);
 		$cleanMessage = TextFormat::clean($message);
 
 		if(!Terminal::hasFormattingCodes()){
